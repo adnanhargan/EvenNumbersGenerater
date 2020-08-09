b400aa69dd89a47a84753e197dce1da6bf9f0738
adnanhargan <adnanhargan101@gmail.com>
2020-08-09 11:26:49 +0500

    Added Fuctionality

--- EvenNumbersGenerater/Program.cs
+++ EvenNumbersGenerater/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace EvenNumbersGenerater
 {
@@ -6,7 +7,46 @@ namespace EvenNumbersGenerater
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hy, we are going to find all the even numbers between two numbers \n Please enter the Lower Limit");
+            int firstInt = int.Parse(Console.ReadLine());
+            Console.WriteLine("Please enter the Upper Limit ");
+            int lastInt = int.Parse(Console.ReadLine());
+
+            //Invoking the Generator Method
+            GenetatoMethod(firstInt, lastInt);
+        }
+        public static void GenetatoMethod(int lowerLimit, int upperLimit)
+        {
+            int counter = 0;
+            var mylist = new List<int>();
+
+            while (lowerLimit <= upperLimit)
+            {
+                if (counter == 0)
+                {
+                    if (lowerLimit % 2 == 0)
+                    {
+                        mylist.Add(lowerLimit);
+                        counter++;
+                        lowerLimit += 2;
+                    }
+                    else
+                    {
+                        lowerLimit += 1;
+                    }
+                }
+                if (counter > 0 && lowerLimit % 2 == 0)
+                {
+                    mylist.Add(lowerLimit);
+                    lowerLimit += 2;
+                    counter++;
+                }
+
+            }
+            foreach (var i in mylist)
+            {
+                Console.WriteLine(i);
+            }
         }
     }
 }
